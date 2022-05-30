9b2ef9f0cc17d4cca0885a3b6219107ce700de10
yannelly25 <yannellym@gmail.com>
2022-05-30 16:41:54 -0400

    styled components added

---
+++ src/pages/Upload.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import UploadForm from '../components/UploadForm';
+
+function Upload() {
+  return (
+    <div>
+      <UploadForm />
+    </div>
+  );
+}
+
+export default Upload;
