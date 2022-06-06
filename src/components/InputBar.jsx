fbd4dce64eb7f76fcb9fc1f26e4397537d564b5c
yannelly25 <yannellym@gmail.com>
2022-06-06 19:05:39 -0400

    addition to input bar

--- src/components/InputBar.jsx
+++ src/components/InputBar.jsx
@@ -5,23 +5,29 @@ const Bar = styled.div`
   width: 90%;
   height: 8rem;
   margin: 5% auto;
-  border: 1px solid black;
+  border: 5px solid black;
   display: flex;
   div{
     width: 80%;
-    border: red solid;
   }
   section{
     display: flex;
     flex-direction: row-reverse;
     width: 10%;
-    border: red solid;
   }
   .deleteDiv{
-    
+    border-right: 5px solid black;
+  }
+  .deleteBtn{
+    width: 100%;
   }
   .checkDiv{
     background-color: black;
+  }
+  .checkBtn{
+    width: 100%;
+    height: 80%;
+    margin-top: 13%;
   }
 `;
 
@@ -29,8 +35,12 @@ function InputBar() {
   return (
     <Bar>
       <div>Input bar</div>
-      <section className="deleteDiv">DELETE</section>
-      <section className="checkDiv">CHECK</section>
+      <section className="deleteDiv">
+        <img src="https://img.icons8.com/external-basicons-solid-edtgraphics/150/undefined/external-delete-ui-elements-basicons-solid-edtgraphics-2.png" className="deleteBtn" alt="delete button" />
+      </section>
+      <section className="checkDiv">
+        <img src="https://img.icons8.com/color/150/undefined/checked--v1.png" className="checkBtn" alt="check button" />
+      </section>
     </Bar>
   );
 }
