e78cda87977c09a17640afad0e4acae71bab733f
yannelly25 <yannellym@gmail.com>
2022-07-08 17:43:55 -0400

    working on bug

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -130,7 +130,7 @@ export default function Profile() {
       name: informationData.name,
       age: informationData.age,
     });
-    setTimeout(() => window.location = '/profile', 600);
+    window.location.reload(true);
   };
 
   const getData = async () => {
