f7df503a98dcf78fc1d393138e3dbe02b2974de5
yannelly25 <yannellym@gmail.com>
2022-07-09 13:24:58 -0400

    testing redirect folder

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import { useHistory } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import { collection, getDocs, doc, updateDoc } from 'firebase/firestore';
 import { useAuth, upload, database } from './firebaseConfig';
 import DefaultProfile from '../assets/images/profilephoto.png';
@@ -97,7 +97,6 @@ export default function Profile() {
   const [show, setShow] = useState(false);
   const [informationData, setInformationData] = useReducer(formReducer, {});
   const [userInformation, setUserInformation] = useState('' || []);
-  const history = useHistory();
 
   const handleClose = () => {
     setShow(false);
@@ -132,7 +131,7 @@ export default function Profile() {
       name: informationData.name,
       age: informationData.age,
     });
-    history.push('/profile');
+      <Navigate to="/profile" replace />;
   };
 
   const getData = async () => {
