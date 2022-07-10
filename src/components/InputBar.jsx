e5bf438281f8ed536dc1b53b67769bffd3bd4647
yannelly25 <yannellym@gmail.com>
2022-07-10 18:59:43 -0400

    testing

--- src/components/InputBar.jsx
+++ src/components/InputBar.jsx
@@ -74,7 +74,7 @@ function InputBar({ curUser }) {
           <img src="https://img.icons8.com/external-basicons-solid-edtgraphics/150/undefined/external-delete-ui-elements-basicons-solid-edtgraphics-2.png" className="bar-btn deleteBtn" alt="delete button" />
         </button>
       </section>
-      <section className="checkDiv" onTouchStart={synthesizeToSpeaker}>
+      <section className="checkDiv" onClick={synthesizeToSpeaker}>
         <button type="button">
           <img src="https://img.icons8.com/material-rounded/96/000000/speaker.png" className="bar-btn checkBtn" alt="speaker button" />
         </button>
