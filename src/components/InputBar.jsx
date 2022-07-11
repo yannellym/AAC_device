a41b3eb37a19a871e783c8ba0a378b0a3a29b2f4
yannelly25 <yannellym@gmail.com>
2022-07-10 22:10:18 -0400

    testing

--- src/components/InputBar.jsx
+++ src/components/InputBar.jsx
@@ -74,11 +74,9 @@ function InputBar({ curUser }) {
           <img src="https://img.icons8.com/external-basicons-solid-edtgraphics/150/undefined/external-delete-ui-elements-basicons-solid-edtgraphics-2.png" className="bar-btn deleteBtn" alt="delete button" />
         </button>
       </section>
-      <section className="checkDiv" onClick={(e) => e.stopPropagation()}>
-        <button type="button" onClick={synthesizeToSpeaker}>
-          <img src="https://img.icons8.com/material-rounded/96/000000/speaker.png" className="bar-btn checkBtn" alt="speaker button" />
-        </button>
-      </section>
+      <button type="button" onClick={synthesizeToSpeaker} className="checkDiv">
+        <img src="https://img.icons8.com/material-rounded/96/000000/speaker.png" className="bar-btn checkBtn" alt="speaker button" />
+      </button>
     </div>
   );
 }
