8907aa2aff171315fa2b83ccf75a08b8c07377ff
yannelly25 <yannellym@gmail.com>
2022-07-11 13:53:09 -0400

    testing

--- src/components/InputBar.jsx
+++ src/components/InputBar.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable react/jsx-props-no-spreading */
 import React, { useState } from 'react';
@@ -76,8 +77,8 @@ function InputBar({ curUser }) {
           <img src="https://img.icons8.com/external-basicons-solid-edtgraphics/150/undefined/external-delete-ui-elements-basicons-solid-edtgraphics-2.png" className="bar-btn deleteBtn" alt="delete button" />
         </button>
       </section>
-      <button type="button" className="checkDiv" onClick={synthesizeToSpeaker}>
-        <img src="https://img.icons8.com/material-rounded/96/000000/speaker.png" className="bar-btn checkBtn" alt="speaker button" />
+      <button type="button" className="checkDiv">
+        <img src="https://img.icons8.com/material-rounded/96/000000/speaker.png" className="bar-btn checkBtn" alt="speaker button" onClick={synthesizeToSpeaker} />
       </button>
     </div>
   );
