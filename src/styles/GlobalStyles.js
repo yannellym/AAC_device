220f77c2fea8af92e9a43e72a2383906b269d2ac
yannelly25 <yannellym@gmail.com>
2022-07-11 13:44:25 -0400

    testing

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -7,6 +7,7 @@ const GlobalStyles = createGlobalStyle`
         box-sizing: border-box;
         margin: 0;
         padding: 0;
+        cursor: pointer;
     }
     body{
         font-family: "Darker Grotesque", "Palanquin Dark", sans-serif;
